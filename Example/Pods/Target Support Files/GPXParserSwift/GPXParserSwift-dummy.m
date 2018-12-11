642c717b220b85f9b5d4e1a723a26750a633c248
cyborgthefirst <johannes@ebelingfs.de>
2018-12-11 19:21:49 +0100

    clean up

---
+++ Example/Pods/Target Support Files/GPXParserSwift/GPXParserSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GPXParserSwift : NSObject
+@end
+@implementation PodsDummy_GPXParserSwift
+@end
---
+++ Example/Pods/Target Support Files/Pods-GPXParserSwift_Tests/Pods-GPXParserSwift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GPXParserSwift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GPXParserSwift_Tests
+@end

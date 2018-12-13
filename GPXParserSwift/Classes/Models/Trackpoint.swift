19d1c815e05971b1e49093a39c0e44f80a89e721
cyborgthefirst <johannes@ebelingfs.de>
2018-12-13 14:25:09 +0100

    Add Trackpoint model class to represent individual point entries of a track

---
+++ GPXParserSwift/Classes/Models/Trackpoint.swift
@@ -0,0 +1,12 @@
+import CoreLocation
+
+public class Trackpoint: Fix {
+    
+    var latitude: Double
+    var longitude: Double
+    
+    init() {
+        latitude = 0.0
+        longitude = 0.0
+    }
+}

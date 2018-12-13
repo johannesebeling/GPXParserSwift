668ca04bac60472f786712351668a6a9c518282b
cyborgthefirst <johannes@ebelingfs.de>
2018-12-13 14:25:23 +0100

    Add Routepoint model class to represent individual point entries of a route

---
+++ GPXParserSwift/Classes/Models/Routepoint.swift
@@ -0,0 +1,21 @@
+public class Routepoint: Fix {
+    
+    // MARK: - Fix protocol
+    
+    var latitude: Double
+    var longitude: Double
+    
+    // MARK: - Properties
+    
+    var name: String
+    var desc: String
+    
+    // MARK: - Initializer
+    
+    init() {
+        latitude = 0.0
+        longitude = 0.0
+        name = ""
+        desc = ""
+    }
+}

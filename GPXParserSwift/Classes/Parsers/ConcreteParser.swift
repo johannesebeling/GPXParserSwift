8a9ab0fcd8efe74cfa0f8c7281b8847e1946e7f0
cyborgthefirst <johannes@ebelingfs.de>
2018-12-13 14:32:44 +0100

    Implement ConcreteParser to ensure XMLParserDelegate functionality which can not be stored in a protocol extension

---
+++ GPXParserSwift/Classes/Parsers/ConcreteParser.swift
@@ -0,0 +1,47 @@
+import MapKit
+import Foundation
+
+public class ConcreteParser<K: Response>: NSObject, Parser {
+    public typealias T = K
+    
+    // MARK: - Parser protocol
+    
+    public var currentString: String?
+    public var result: K?
+    public var completion: ((K?) -> Void)?
+    
+    // MARK: - Initializer
+    
+    override required public init() {}
+    
+    // MARK: - XMLParser Delegate
+    
+    @objc func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if currentString == nil {
+            currentString = ""
+        }
+        currentString?.append(string)
+    }
+    
+    @objc func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        DispatchQueue.main.async {
+            self.completion?(nil)
+        }
+    }
+    
+    @objc func parser(_ parser: XMLParser, validationErrorOccurred validationError: Error) {
+        DispatchQueue.main.async {
+            self.completion?(nil)
+        }
+    }
+    
+    @objc func parserDidStartDocument(_ parser: XMLParser) {
+        result = T()
+    }
+
+    @objc func parserDidEndDocument(_ parser: XMLParser) {
+        DispatchQueue.main.async {
+            self.completion?(self.result)
+        }
+    }
+}

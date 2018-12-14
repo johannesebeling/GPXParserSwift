573548d7a463c488d162f247a3a9b7e98cd1e492
cyborgthefirst <johannes@ebelingfs.de>
2018-12-14 11:51:32 +0100

    Added base implementation of the coordinate protocol

---
+++ GPXParserSwift/Models/Point.swift
@@ -0,0 +1,10 @@
+public struct Point: Coordinate {
+    
+    public var latitude: Double
+    public var longitude: Double
+    
+    public init() {
+        latitude = 0.0
+        longitude = 0.0
+    }
+}

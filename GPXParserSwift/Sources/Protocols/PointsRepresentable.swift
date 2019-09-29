e16b8fc17068c192a9c593e17c311f7750cc4bb6
Johannes Ebeling <johannes@ebelingfs.de>
2019-09-29 15:38:20 +0200

    #2 Remove all previous files

--- GPXParserSwift/Sources/Protocols/PointsRepresentable.swift
+++
@@ -1,7 +0,0 @@
-public protocol PointsRepresentable {
-    associatedtype T: Coordinate
-    
-    var points: [T] { get set }
-    var distance: Double { get set }
-    var region: CoordinateRegion { get set }
-}
